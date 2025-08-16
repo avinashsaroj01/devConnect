faa3f1f9d764b9e10dc9caf949a3244feb419faa
Avinash Saroj <avinashsarojofficial@gmail.com>
2025-08-16 19:36:12 +0530

    fetched data from jsonplaceholde r external  api and populated  name inside Developers component.

--- src/pages/Developers.js
+++ src/pages/Developers.js
@@ -1,32 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { developers } from "../data";
 function Developers() {
   const [searchText, setSearchText] = useState("");
+  const [usersData, setUsersData] = useState([]);
   const handleSearch = (e) => {
     setSearchText(e.target.value);
     console.log("SearchText", searchText);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+          throw new Error(` HTTP error occured : ${response.status}`);
+        }
+
+        const data = await response.json();
+        setUsersData(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log("Api data : ", usersData);
+
   return (
     <div className="devList">
-      <div style={{width:'45%'}} >
+      <div style={{ width: "50%" }}>
         <h1>Developer List</h1>
 
         <div className="devItem">
-          {developers
+          {usersData
             .filter(
               (devs) =>
                 searchText.length === 0 ||
-                (Array.isArray(devs.skills) &&
-                  devs.skills.some((skill) =>
-                    skill.toLowerCase().includes(searchText.toLowerCase())
-                  ))
+                devs.name.toLowerCase().includes(searchText.toLowerCase())
             )
             .map((item) => {
               return (
-                <div className="devInfo">
-                  {<div className="name">{item.name}</div>}
-                  {<div className="email">{item.email}</div>}
-                  {<div className="skills">{item.skills.join(" , ")}</div>}
+                <div key={item.id} className="devInfo">
+                  {<h3 className="name">{item.name}</h3>}
+                  {/* {<p className="email">Email : {item.email}</p>}
+                  {<p className="skills">{item.username}</p>}
+                  {<p className="skills">Street : {item.address.street}</p>}
+                  {<p className="skills">City : {item.address.city}</p>}
+                  {<p className="skills">Company : {item.company.name}</p>}
+                  {
+                    <p className="skills">
+                      Company Website : {item.website}
+                    </p>
+                  } */}
                 </div>
               );
             })}
